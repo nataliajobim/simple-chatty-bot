98967c404377b82e7feb2bb5c97aa3f2f5da7c85
Natália Jobim <109051290+nataliajobim@users.noreply.github.com>
2023-01-11 23:11:41 -0300

    Create parte2.kt

---
+++ parte2.kt
@@ -0,0 +1,11 @@
+package bot
+
+fun main() {
+    println("Hello! My name is Kyara.")
+    println("I was created in 2023.")
+    println("Please, remind me your name.")
+
+    val yourName = readln()
+
+    println("What a great name you have, $yourName!")
+}
